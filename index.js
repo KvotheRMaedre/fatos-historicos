9dc05912d72f58ce08b4635219fb1f3f540a473e
Ítalo H <italodssouza.backup@gmail.com>
2023-02-16 13:30:42 -0300

    Update index.js - Api fato-historico/:ano
    Criando a api para buscar os fatos pelo ano informado.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+const fatoHistorico = require('./fato_historico');
+
+app.get('/fato-historico/:ano', (req, res) => {
+    let ano = req.params.ano;
+    res.json(fatoHistorico.buscarFatoPorAno(ano));
+});
+
+app.listen(8080, () => {
+})
