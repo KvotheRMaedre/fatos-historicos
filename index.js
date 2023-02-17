d281dbc82c93ebcb8a0b48332532e05f8e294ee5
Ítalo H <italodssouza.backup@gmail.com>
2023-02-17 07:28:22 -0300

    Update index.js
    Adicionando validação do campo ano

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express();
 const fatoHistorico = require('./fato_historico');
 
 app.get('/fato-historico/:ano', (req, res) => {
-    let ano = req.params.ano;
+    
+    let ano = parseInt(req.params.ano, 10);
+
+    if(ano < 1920 || ano > 2020){
+        res.status(400).json('{O ano deve estar entre 1920 e 2020}');
+    }
     res.json(fatoHistorico.buscarFatoPorAno(ano));
 });
 
